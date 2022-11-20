d32fac8dfbd893318152765c3550bf14ad4ee6c4
chinmaiveera <47609634+chinmaiveera@users.noreply.github.com>
2022-11-20 21:44:13 +0530

    Add files via upload

---
+++ Assignment-14/LowestCommonAncestor.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+//below code executed and accepted on leetcode.
+//Lowest Common Ancestor of a Binary Search Tree
+class LowestCommonAncestor {
+    //below function takes the root node and other two node part of same tree and
+    //return the common ancestor of given two nodes.
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //declare queues "q1","q2" that store all the ancestor nodes of "p" and "q" respectively
+        Queue<TreeNode> q1 = findabove(root,p);
+        Queue<TreeNode> q2 = findabove(root,q);
+        TreeNode prev=root;
+        //run loop trough both queues parellely and find the last common ancestor of both nodes.
+        while(q1.peek() == q2.peek()){
+            prev = q1.peek();
+            q1.remove();
+            q2.remove();
+        }
+        //return that ancestor.
+        return prev;
+    //TIME COMPLEXITY:
+    //  for finding all ancestors: 
+    //    maximum no of ancestors a node can have is (h-1),where h is height of tree.
+    //  for finding last common ancestor: 
+    //    h,as queue contains all ancestors and
+    //    max no of  ancestors a node can have is (h-1),where h is height of tree.
+    //So,time complexity is O(h),where h is height of tree.
+    //BEST/AVG CASE:
+    //    least/avg height of bst can be is logn where n is total no of elements.
+    //    So,O(h) = O(logn)
+    //WORST CASE:
+    //    maximum height bst can have is logn where n is total no of elements.
+    //    So,O(h) = O(n)
+    }
+    
+    
+    //below function takes the root node and target node,
+    //returns queue containing all ancestors of target node.
+    public Queue<TreeNode> findabove(TreeNode root,TreeNode target){
+        Queue<TreeNode> que = new LinkedList<TreeNode>();
+        while(root!=null){
+            que.add(root);
+            if(root.val == target.val)
+                break;
+            if(root.val > target.val)
+                root = root.left;
+            else
+                root = root.right;
+        }
+        return que;
+    }
+}
---
+++ Assignment-14/RecoverTree.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//below code executed and accepted on leetcode.
+//Recover Binary Search Tree
+class RecoverTree {
+    //this function takes the root node of tree and rearrange the two swapped elements
+    public void recoverTree(TreeNode root) {
+        //create an array list that stores the nodes
+        ArrayList<TreeNode> arr = new ArrayList<TreeNode>();
+        //below function takes the root of tree and empty array.
+        //Then fill the array with tree nodes in inorder of tree.
+        inorder(root,arr);
+        TreeNode first=null;
+        TreeNode second=null;
+        //we know that exactly two elements are misplaced and 
+        //in bst inorder of a tree is nothing but ascending order of values of its nodes.
+        //So,we loop trough the array and store the misplaced nodes in "first","second" variables.
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr.get(i).val>arr.get(i+1).val){
+                if(first == null){
+                    first = arr.get(i);
+                    second = arr.get(i+1);
+                }
+                else
+                    second = arr.get(i+1);
+            }
+        }
+        //now swap the values of "first" and "second" nodes.
+        swap(first,second);
+        //TimeComplexity:
+        //for Inorder traversal: n,where n is total elements of tree.
+        //for searching:n,where n is total elements of tree.
+        //for swapping:1
+        //Therefore TimeComplexity will be n+n+1 ~ O(n)
+        
+    
+    }
+    //below function takes the root of tree and empty array.
+    //Then fill the array with tree nodes in inorder of tree.
+    public void inorder(TreeNode root,ArrayList<TreeNode> arr){
+        if(root==null)
+            return;
+        inorder(root.left,arr);
+        arr.add(root);
+        inorder(root.right,arr);
+    }
+    //below function takes the two nodes and swap their values.
+    public void swap(TreeNode first,TreeNode second){
+        int temp;
+        temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+}
